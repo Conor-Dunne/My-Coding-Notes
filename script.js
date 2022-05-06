10ede1e1579c5b3672b3071a8025c469e064d72d
Conor Dunne <conordunne@outlook.ie>
2022-05-06 16:57:43 +0900

    Add literal objects to use in test of displaying in html via JS

---
+++ script.js
@@ -0,0 +1,36 @@
+let notes = [
+    {
+        title: "Array.filter1",
+        summary: "Filter an array for elements that pass a tes",
+        points: ["this is point one", "point two"],
+        code: {
+            line1: "const ages = [10,15,67,40,23];",
+            line2: "ages.filter(age => age =<15);",
+            line3: "result: [15,67,40,23]",
+        },
+    },
+    {
+        title: "Array.filter2",
+        summary: "Filter an array for elements that pass a tes",
+        points: ["this is point one", "point two"],
+        code: {
+            line1: "const ages = [10,15,67,40,23];",
+            line2: "ages.filter(age => age =<15);",
+            line3: "result: [15,67,40,23]",
+        },
+    },
+    {
+        title: "Array.filter3",
+        summary: "Filter an array for elements that pass a tes",
+        points: ["this is point one", "point two"],
+        code: {
+            line1: "const ages = [10,15,67,40,23];",
+            line2: "ages.filter(age => age =<15);",
+            line3: "result: [15,67,40,23]",
+        },
+    }
+
+]
+
+
+
